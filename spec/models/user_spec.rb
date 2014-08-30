25b3d9d43fca0d3742f21162f1622aa38e30a5d3
Stephens Xu <stephensxub@gmail.com>
2014-08-29 23:42:31 -0700

    using annotate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
