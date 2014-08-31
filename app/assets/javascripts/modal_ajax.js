1c0f9b0b2ac966e861fa277add5b734d3c7efa1c
Stephens Xu <stephensxub@gmail.com>
2014-08-30 23:47:34 -0700

    added class to forms

--- app/assets/javascripts/modal_ajax.js
+++ app/assets/javascripts/modal_ajax.js
@@ -9,10 +9,11 @@ $.ajaxSetup({
 $(document).ready(function() {
   console.log("document is ready for use");
 
-  $(document).bind('ajax:success', 'my-modal-content', function(data, status, xhr) {
+  $(document).bind('ajax:complete', function(event, data) {
     console.log("ajax is success");
     console.log(xhr);
     console.log(data);
     console.log(status);
+    $('').html(data.responseText);
   });
 });
