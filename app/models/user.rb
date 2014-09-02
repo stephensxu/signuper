c4f97b0c65fe12a1bce73c77337cadd1629b6a2b
Stephens Xu <stephensxub@gmail.com>
2014-09-01 23:16:06 -0700

    rails generate devise User

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@
 #
 
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_secure_password
   
   validates :email, :presence => true, :uniqueness => true, :length => { :minimum => 6 }, :email => true
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@
 
 Rails.application.routes.draw do
 
+  devise_for :users
   root :to => 'users#index'
 
   resources :users, :except => :destroy
