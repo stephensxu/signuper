f9db57ff6e2f550032c5dc0d73518ad7b13101c9
Stephens Xu <stephensxub@gmail.com>
2014-09-02 00:13:20 -0700

    added amniauthable to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
   has_secure_password
   
   validates :email, :presence => true, :uniqueness => true, :length => { :minimum => 6 }, :email => true
