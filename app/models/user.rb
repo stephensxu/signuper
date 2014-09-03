7f31c53844c71d9f26817564e3fcf85f12f56ec1
Stephens Xu <stephensxub@gmail.com>
2014-09-03 16:37:55 -0700

    deleted extra columns added by devise by my own mistake in master branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,28 +2,16 @@
 #
 # Table name: users
 #
-#  id                     :integer          not null, primary key
-#  email                  :string(255)      default(""), not null
-#  password_digest        :string(255)      not null
-#  nickname               :string(255)      not null
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  encrypted_password     :string(255)      default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :inet
-#  last_sign_in_ip        :inet
-#  provider               :string(255)
-#  uid                    :string(255)
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  password_digest :string(255)
+#  nickname        :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 # Indexes
 #
-#  index_users_on_email                 (email) UNIQUE
-#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_email  (email) UNIQUE
 #
 
 
