e66be90df12b0545290d5bee4e3337ed1b248861
Stephens Xu <stephensxub@gmail.com>
2014-09-04 01:11:08 -0700

    added resize_to_fit and resize_to_fill to avatar_uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,13 @@
 # encoding: utf-8
 
 class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  process :resize_to_fit => [800, 800]
+
+  version :thumb do
+    process :resize_to_fill => [200, 200]
+  end
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
