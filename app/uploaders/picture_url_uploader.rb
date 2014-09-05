660d5e998a2493d04c174ecd053a68ddb4c2ff2d
Stephens Xu <stephensxub@gmail.com>
2014-09-04 23:18:37 -0700

    deleted avatar_uploader and generated picture_url_uploader

--- app/uploaders/picture_url_uploader.rb
+++ app/uploaders/picture_url_uploader.rb
@@ -1,24 +1,13 @@
 # encoding: utf-8
 
-class AvatarUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-  
-  process :resize_to_fit => [800, 800]
-
-  version :thumb do
-    process :resize_to_fill => [200, 200]
-  end
+class PictureUrlUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -59,7 +48,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
 end
