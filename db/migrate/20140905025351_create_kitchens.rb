38564158fd242d336b49da8328e733ca32399a56
Stephens Xu <stephensxub@gmail.com>
2014-09-04 21:46:21 -0700

    deleted kitchen model and controller

--- db/migrate/20140905025351_create_kitchens.rb
+++
@@ -1,9 +0,0 @@
-class CreateKitchens < ActiveRecord::Migration
-  def change
-    create_table :kitchens do |t|
-      t.string :photo, null: false
-
-      t.timestamps
-    end
-  end
-end
--- spec/controllers/kitchens_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe KitchensController, :type => :controller do
-
-end
