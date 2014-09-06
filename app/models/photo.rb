2a2483645eceedead6a861737dc44f39ab78da82
Stephens Xu <stephensxub@gmail.com>
2014-09-05 17:28:20 -0700

    file_size validation now working for 2MG upload limit; changed content in config/locales/en.yml

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -13,10 +13,12 @@
 #  index_photos_on_user_id  (user_id)
 #
 
+require 'file_size_validator'
+
 class Photo < ActiveRecord::Base
   belongs_to :user
 
   mount_uploader :picture_url, PhotoUploader
   validates :picture_url, :presence => true, 
-            :file_size => { :maximum => 0.1.megabyes.to_i }
+            :file_size => { :maximum => 2.megabytes.to_i }
 end
