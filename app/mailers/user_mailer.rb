ac67b5f300c9ba16c21064e1e54dac8000410512
Stephens Xu <stephensxub@gmail.com>
2014-09-06 01:07:05 -0700

    added welcome_email to UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "hell@couchfoodie.com"
+
+  def welcome_email
+    mail :subject => "Mandrill rides the Rails!",
+         :to      => current_user.email,
+         :from    => "hello@couchfoodie.com"
+  end
 end
